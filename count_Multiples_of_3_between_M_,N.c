273dfaa5a4ebf0d9f0be813a8ff157407afffa75
badithaboinaAkshay <120081282+badithaboinaAkshay@users.noreply.github.com>
2022-12-19 21:20:46 +0530

    #include<stdio.h> int main() {     int n,m,i,count=0;     scanf("%d%d",&m,&n);     for(i=m;i<=n;i++)     {         if(i%3==0)         {             count++;         }
        }
        printf("%d",count);
    }

---
+++ count_Multiples_of_3_between_M_,N.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,i,count=0;
+    scanf("%d%d",&m,&n);
+    for(i=m;i<=n;i++)
+    {
+        if(i%3==0) 
+        {
+            count++;
+        }
+        
+    }
+    printf("%d",count);
+}
