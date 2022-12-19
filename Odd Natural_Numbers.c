bf3680821c23f51933f312fcd5e0eae50d1a7160
badithaboinaAkshay <120081282+badithaboinaAkshay@users.noreply.github.com>
2022-12-19 20:55:42 +0530

    #include<stdio.h> int main() {     int i,n,odd;     scanf("%d",&n);     for (i=1;i<=n;i++)     {         if (i%2!=0)         {             printf("%d ",i);         }     } }

---
+++ Odd Natural_Numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,odd;
+    scanf("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        if (i%2!=0)
+        {
+            printf("%d ",i);
+        }
+    }
+}
