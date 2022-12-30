99f44a5e72153566fa952d99b799b2676a56d6e1
badithaboinaAkshay <120081282+badithaboinaAkshay@users.noreply.github.com>
2022-12-30 15:20:24 +0530

    #include<stdio.h> int main() {     int n,m,max=0,i;     scanf("%d%d",&n,&m);     for(i=m;i;i++)     {         if(i%n==0&&i%m==0)         {             printf("%d",i);             break;         }     } }

--- LCM_of_two_given_numbers.c
+++ LCM_of_two_given_numbers.c
@@ -1,20 +1,14 @@
-#include <stdio.h>
-
-int main() {
-
-    int n1, n2, max;
-
-      scanf("%d %d", &n1, &n2);
-
-    // maximum number between n1 and n2 is stored in max
-    max = (n1 > n2) ? n1 : n2;
-
-    while (1) {
-        if ((max % n1 == 0) && (max % n2 == 0)) {
-            printf("%d",max);
+#include<stdio.h>
+int main()
+{
+    int n,m,max=0,i;
+    scanf("%d%d",&n,&m);
+    for(i=m;i;i++)
+    {
+        if(i%n==0&&i%m==0)
+        {
+            printf("%d",i);
             break;
         }
-        ++max;
     }
-    return 0;
 }
