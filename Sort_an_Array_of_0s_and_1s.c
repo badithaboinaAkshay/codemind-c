99e626797bd9e65f4c1c459576dfc180b49b08c3
badithaboinaAkshay <120081282+badithaboinaAkshay@users.noreply.github.com>
2022-12-31 12:30:05 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n;     cin>>n;     int array[n];     for(int i=0;i<n;++i){         cin>>array[i];     }     sort(array,array+n);     for(auto x:array){         cout<<x<<" ";     } }

---
+++ Sort_an_Array_of_0s_and_1s.c
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int array[n];
+    for(int i=0;i<n;++i){
+        cin>>array[i];
+    }
+    sort(array,array+n);
+    for(auto x:array){
+        cout<<x<<" ";
+    }
+}
